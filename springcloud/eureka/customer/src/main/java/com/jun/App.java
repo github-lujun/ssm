eeb598d47d33a3b2c0928c0cb507f636b132feb3
lujun <lujun@izuche.com>
2019-09-20 11:23:57 +0800

    fix

--- springcloud/eureka/customer/src/main/java/com/jun/App.java
+++ springcloud/eureka/customer/src/main/java/com/jun/App.java
@@ -5,9 +5,13 @@ import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+
 
 @SpringBootApplication
 @RestController
@@ -18,9 +22,14 @@ public class App {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder){
+        return builder.build();
+    }
+
     @RequestMapping("/hello")
     public String hello(){
-        return this.restTemplate.getForObject("DESKTOP-LQ84CS8.lan:eureka.client:8762",String.class);
+        return this.restTemplate.getForObject("http://windows10.microdone.cn:8762/",String.class);
     }
 
     public static void main(String[] args){
