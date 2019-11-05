fcbc98c2cd9767285037585fcd675acbf0b313e3
lujun <lujun@izuche.com>
2019-11-05 17:32:50 +0800

    添加过滤器

--- javaweb/src/main/java/com/jun/filter/HttpServletRequestFilter.java
+++ javaweb/src/main/java/com/jun/filter/HttpServletRequestFilter.java
@@ -11,7 +11,11 @@ public class HttpServletRequestFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-
+        //执行前
+        //todo:字符编码
+        chain.doFilter(request,response);
+        //执行后操作
+        //todo:字符编码
     }
 
     @Override
