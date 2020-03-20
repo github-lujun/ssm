3e9765b52fb55e85140c584136eda743abdcf24a
lujun <838587241@qq.com>
2020-03-20 15:32:17 +0800

    springautowire:添加scope作用域

--- springautowire/src/main/java/com/jun/service/impl/PersonServiceImpl.java
+++ springautowire/src/main/java/com/jun/service/impl/PersonServiceImpl.java
@@ -4,9 +4,16 @@ import com.jun.dao.PersonDao;
 import com.jun.pojo.Person;
 import com.jun.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
+/**
+ * @Scope bean的作用域
+ * */
 @Component
+/*@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)*/
 public class PersonServiceImpl implements PersonService {
     @Autowired
     private PersonDao personDao;
