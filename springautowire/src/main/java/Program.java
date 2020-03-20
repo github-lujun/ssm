23ebf0b3be0f8e21161b3c3cbdb861e5ac84e6d1
lujun <838587241@qq.com>
2020-03-20 13:38:26 +0800

    springautowire：添加xml自动装配方式

--- springautowire/src/main/java/Program.java
+++ springautowire/src/main/java/Program.java
@@ -3,15 +3,18 @@ import com.jun.pojo.Person;
 import com.jun.service.PersonService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * @ComponentScan
+ * @ComponentScan or <context:component-scan base-package="com.jun"></context:component-scan>
  * @Component
  * @Autowire
  * */
 public class Program {
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(Config.class);
+        ApplicationContext applicationContext = null;
+        applicationContext = new AnnotationConfigApplicationContext(Config.class);
+        //applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
         PersonService personService = applicationContext.getBean(PersonService.class);
         Person person = personService.getPerson(1);
         System.out.println(person);
