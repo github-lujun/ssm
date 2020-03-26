8ff55aa8e1c53d55dee2d7d727c2f3e33c10db08
lujun <838587241@qq.com>
2020-03-26 22:55:53 +0800

    add shiro module

--- shiro/src/main/java/Program.java
+++ shiro/src/main/java/Program.java
@@ -1,8 +1,29 @@
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+
 /**
  * https://www.w3cschool.cn/shiro/
  * */
 public class Program {
     public static void main(String[] args) {
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("leo", "root");
+        try {
+            subject.login(token);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
+        System.out.println(subject.isAuthenticated());
+
+        subject.logout();
+        System.out.println(subject.isAuthenticated());
     }
 }
