0d4247494c82b64fbb9a0748783a0438b697c5b9
lujun <838587241@qq.com>
2020-03-29 14:24:49 +0800

    shiro:authorizer

--- shiro/src/main/java/Program.java
+++ shiro/src/main/java/Program.java
@@ -2,8 +2,11 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.util.Factory;
+
+import java.util.List;
 
 /**
  * https://www.w3cschool.cn/shiro/
@@ -11,7 +14,7 @@ import org.apache.shiro.util.Factory;
 public class Program {
     public static void main(String[] args) {
         //Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
-        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro-authenticator-all-success.ini");
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro-permission.ini");
         SecurityManager securityManager = factory.getInstance();
         SecurityUtils.setSecurityManager(securityManager);
         Subject subject = SecurityUtils.getSubject();
@@ -23,13 +26,31 @@ public class Program {
             e.printStackTrace();
         }
 
-        int size = subject.getPrincipals().asList().size();
+        /*System.out.println("login:");
+        System.out.println("isAuthenticated: "+subject.isAuthenticated());
+        PrincipalCollection principals = subject.getPrincipals();
+        List list = principals.asList();
+        int size = list.size();
+        System.out.println("principals: "+size);
+        //rbc基于角色的访问控制
+        boolean hasRole = subject.hasRole("role1");
+        System.out.println("role: "+hasRole);
+        //rbc基于资源的访问控制（资源:操作:对象实例ID）
+        boolean permittedAll = subject.isPermittedAll("user:create", "user:update", "user:delete");
+        System.out.println("permittedAll: "+permittedAll);*/
+        //单个资源单个权限
+        /*boolean permitted = subject.isPermitted("user:create");
+        System.out.println("permitted: "+permitted);*/
+        //单个资源多个权限
+        boolean[] permitted = subject.isPermitted("user:create","user:update");
+        System.out.println("permitted0: "+permitted[0]+"\npermitted1: "+permitted[1]);
 
-        System.out.println(size);
-
-        //System.out.println(subject.isAuthenticated());
-
-        //subject.logout();
-        //System.out.println(subject.isAuthenticated());
+        /*subject.logout();
+        System.out.println("logout");
+        System.out.println("isAuthenticated: "+subject.isAuthenticated());
+        hasRole = subject.hasRole("role1");
+        System.out.println("role: "+hasRole);
+        permittedAll = subject.isPermittedAll("user:create", "user:update", "user:delete");
+        System.out.println("permittedAll: "+permittedAll);*/
     }
 }
