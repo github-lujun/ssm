ad1efd8847941172f049763e958023a0ebae5da7
lujun <838587241@qq.com>
2020-03-30 17:48:12 +0800

    netty

---
+++ netty/src/main/java/com/jun/nio/netty/TimeClient.java
@@ -0,0 +1,52 @@
+package com.jun.nio.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class TimeClient {
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try{
+            Bootstrap b = new Bootstrap();
+            b.group(group).channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY,true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new ChannelHandlerAdapter(){
+                                @Override
+                                public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                    byte[] req = "hello".getBytes();
+                                    ByteBuf firstMessage = Unpooled.buffer(req.length);
+                                    firstMessage.writeBytes(req);
+                                    ctx.writeAndFlush(firstMessage);
+                                }
+
+                                @Override
+                                public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                    ByteBuf buf = (ByteBuf) msg;
+                                    byte[] req = new byte[buf.readableBytes()];
+                                    buf.readBytes(req);
+                                    System.out.println(new String(req));
+                                }
+
+                                @Override
+                                public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+                                    ctx.close();
+                                }
+                            });
+                        }
+                    });
+
+            ChannelFuture f = b.connect("127.0.0.1", 9050).sync();
+            f.channel().closeFuture().sync();
+        }finally {
+            group.shutdownGracefully();
+        }
+    }
+}
---
+++ netty/src/main/java/com/jun/nio/netty/TimeServer.java
@@ -0,0 +1,55 @@
+package com.jun.nio.netty;
+
+import com.sun.corba.se.impl.ior.ByteBuffer;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class TimeServer {
+    public static void main(String[] args) throws InterruptedException {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new ChannelHandlerAdapter() {
+                                @Override
+                                public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                    ByteBuf buf = (ByteBuf) msg;
+                                    byte[] req = new byte[buf.readableBytes()];
+                                    buf.readBytes(req);
+                                    System.out.println(new String(req,"utf-8"));
+
+                                    ByteBuf resp = Unpooled.copiedBuffer("hi".getBytes());
+                                    ctx.write(resp);
+                                }
+
+                                @Override
+                                public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+                                    ctx.flush();
+                                }
+
+                                @Override
+                                public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+                                    ctx.close();
+                                }
+                            });
+                        }
+                    });
+            ChannelFuture f = b.bind(9050).sync();
+            f.channel().closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
+}
