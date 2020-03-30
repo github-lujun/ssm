2dcb706ead9e1fdbbdb36066c5802157c4d4df1a
lujun <838587241@qq.com>
2020-03-30 12:14:30 +0800

    shiro:web

---
+++ shiro/src/main/java/com/jun/controller/ShiroServlet.java
@@ -0,0 +1,26 @@
+package com.jun.controller;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/shiro")
+public class ShiroServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Subject subject = SecurityUtils.getSubject();
+        boolean authenticated = subject.isAuthenticated();
+        System.out.println(authenticated);
+    }
+}
